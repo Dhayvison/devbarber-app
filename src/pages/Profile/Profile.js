dd304b3c6f2837328039f0bfbeb8cf544bb5abd5
Dhayvison Braga <dhayvison.braga@hostweb.com.br>
2021-02-03 20:05:06 -0300

    feat: Profile screen and log out

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,12 +1,53 @@
 import * as React from 'react';
-import {Text} from 'react-native';
-
-import FullScreenContainer from '../../components/FullScreenContainer';
+import {Alert} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+import Api from '../../api';
+import {
+  Wrapper,
+  UserInfo,
+  UserAvatar,
+  UserName,
+  UserEmail,
+  LogOutButton,
+  Strong,
+} from './Profile.style';
+import {UserContext} from '../../contexts/UserContext';
+import {Palette} from '../../utils';
 
 export default () => {
+  const {state: user} = React.useContext(UserContext);
+  const {navigate} = useNavigation();
+
+  async function handleLogOut() {
+    Alert.alert('Sair do Aplicativo', 'Deseja mesmo sair?', [
+      {
+        text: 'Cancelar',
+        onPress: () => {},
+        style: 'cancel',
+      },
+      {
+        text: 'Sair',
+        onPress: async () => {
+          await Api.logOut();
+          await AsyncStorage.setItem('token', '');
+          navigate('Preload');
+        },
+      },
+    ]);
+  }
+
   return (
-    <FullScreenContainer>
-      <Text>Profile</Text>
-    </FullScreenContainer>
+    <Wrapper>
+      <UserInfo source={{uri: user.avatar}} imageStyle={{opacity: 0.1}}>
+        <UserAvatar source={{uri: user.avatar}} />
+        <UserName>{user.name + ' Braga Mendonça'}</UserName>
+        <UserEmail>{user.email}</UserEmail>
+      </UserInfo>
+
+      <LogOutButton onPress={handleLogOut} underlayColor={Palette.red}>
+        <Strong>Sair</Strong>
+      </LogOutButton>
+    </Wrapper>
   );
 };
---
+++ src/pages/Profile/Profile.style.js
@@ -0,0 +1,64 @@
+import {
+  Image,
+  ImageBackground,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import styled from 'styled-components';
+import {Palette} from '../../utils';
+
+export const Wrapper = styled(View)`
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+  padding: 40px;
+  background-color: ${Palette.cyan};
+`;
+
+export const UserInfo = styled(ImageBackground)`
+  width: 100%;
+  align-items: center;
+  padding: 60px 40px;
+  background-color: ${Palette.blue};
+  border-radius: 30px;
+  overflow: hidden;
+`;
+
+export const UserAvatar = styled(Image)`
+  width: 250px;
+  height: 250px;
+  border-width: 10px;
+  border-color: ${Palette.light};
+  border-radius: 125px;
+`;
+
+export const UserName = styled(Text)`
+  font-size: 50px;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export const UserEmail = styled(Text)`
+  color: ${Palette.light};
+  margin-top: 20px;
+`;
+
+export const LogOutButton = styled(TouchableHighlight)`
+  width: 100%;
+  height: 60px;
+  flex-direction: row;
+  border: 2px solid rgba(256, 256, 256, 0.3);
+  border-radius: 30px;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const Strong = styled(Text)`
+  color: ${Palette.dark};
+  font-weight: bold;
+  color: white;
+  text-transform: uppercase;
+`;
